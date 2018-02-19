e8ab6e19f7c5486cd0204105645645c9dd9d9ee8
Sabber Ahamed <sabbers@gmail.com>
2018-02-18 22:29:09 -0600

    Rename files and modify codes little bit

--- create_data.py
+++ create_data.py
@@ -3,17 +3,19 @@ import pandas as pd
 
 ## Import data
 frames = []
+file_name = 'total_raw'
+
 for i in range(400):
-    file_name = "data/data."+ str(i) +".csv"
-    df = pd.read_csv(file_name)
+    file_path = "data/" + str(file_name) + "." + str(i) + ".csv"
+    df = pd.read_csv(file_path)
     frames.append(df)
-    
+
 df = pd.concat(frames)
 
 dropped_cols = ['velocity:2', 'force:2', 'total displacement:2', 'coordinate:2', 'material', 'viscosity', 'effective viscosity',
        'elem number', 'mesh quality', 'node number']
 df = df.drop(dropped_cols, axis=1)
 
-df.to_csv("data/deviatoric_concatenate.csv", index=False)
+df.to_csv("data/" + str(file_name) + "_concatenate.csv", index=False)
 
 print('Finished saving data')
