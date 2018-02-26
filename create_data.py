61f50bca398d0f627f24cdf624b6a5a88dabd603
Sabber Ahamed <sabbers@gmail.com>
2018-02-26 17:32:10 -0600

    Add figure module section

--- create_data.py
+++ create_data.py
@@ -3,19 +3,19 @@ import pandas as pd
 
 ## Import data
 frames = []
-file_name = 'total_raw'
+file_name = 'totoal'
 
-for i in range(400):
-    file_path = "data/" + str(file_name) + "." + str(i) + ".csv"
+for i in range(300):
+    file_path = "new_data/" + str(file_name) + "." + str(i) + ".csv"
     df = pd.read_csv(file_path)
     frames.append(df)
 
 df = pd.concat(frames)
 
 dropped_cols = ['velocity:2', 'force:2', 'total displacement:2', 'coordinate:2', 'material', 'viscosity', 'effective viscosity',
-       'elem number', 'mesh quality', 'node number']
+       'elem number', 'mesh quality']
 df = df.drop(dropped_cols, axis=1)
 
-df.to_csv("data/" + str(file_name) + "_concatenate.csv", index=False)
+df.to_csv("data/total_300.csv", index=False)
 
 print('Finished saving data')
